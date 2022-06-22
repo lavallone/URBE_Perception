70446fd2e30d214cae9f054d7e73d380a5cb338e
lavallone <leo.lava99@gmail.com>
2022-06-22 11:06:56 +0200

    ll

--- toolkit/extraction/src/main_waymo.py
+++ toolkit/extraction/src/main_waymo.py
@@ -28,41 +28,27 @@ if __name__=="__main__":
     
     toolkit = WaymoOpenDataset.ToolKit(training_dir=training_dir, save_dir=save_dir)
     
-    if args.mode == "add":
-        iteration = 0
-        list_segments = toolkit.list_training_segments()
-        num_segments = len(list_segments)
+    
+    iteration = 0
+    list_segments = toolkit.list_training_segments()
+    num_segments = len(list_segments)
         
-        for segment in list_segments: # mi creo una lista di segmenti...
-            iteration = iteration + 1
-            num_segments = num_segments - 1
-            print("^^^^^^^^^^^^^^^^^^^^^^ Starting processing |{}| ^^^^^^^^^^^^^^^^^^^^^^".format(segment[:-28]))
-            if num_segments != 0:
-                print("^^^^^^^^^^^^^^^^^^^^^^     {} segments left     ^^^^^^^^^^^^^^^^^^^^^^".format(num_segments))
-            else:
-                print("^^^^^^^^^^^^^^^^^^^^^^  Last segment to process ^^^^^^^^^^^^^^^^^^^^^^")
-            toolkit.assign_segment(segment)
+    for segment in list_segments: # mi creo una lista di segmenti...
+        iteration = iteration + 1
+        num_segments = num_segments - 1
+        print("^^^^^^^^^^^^^^^^^^^^^^ Starting processing |{}| ^^^^^^^^^^^^^^^^^^^^^^".format(segment[:-28]))
+        if num_segments != 0:
+            print("^^^^^^^^^^^^^^^^^^^^^^     {} segments left     ^^^^^^^^^^^^^^^^^^^^^^".format(num_segments))
+        else:
+            print("^^^^^^^^^^^^^^^^^^^^^^  Last segment to process ^^^^^^^^^^^^^^^^^^^^^^")
+        toolkit.assign_segment(segment)
             
-            process_segment()
-            #toolkit.save_video()
-            #toolkit.consolidate()
+        process_segment()
+        #toolkit.save_video()
+        #toolkit.consolidate()
             
-            if iteration == 100: # for controlling how many segments we're going to process
-                break 
+        if iteration == 100: # for controlling how many segments we're going to process
+            break 
         
-        print("################# Processing is Finished ;) #################")
-        print("Number of processed segments: {}".format(iteration))
-        
-    elif args.mode == "del": # abbiamo intenzione di eliminare la cartelle relative a questi segmenti
-        print("removing the indicated segments directories...")
-        print("[The ones saved in 'archived_files']")
-        list_saved_segments = os.listdir(save_dir)[1:]
-        list_to_delete_segments = toolkit.list_training_segments()
-        
-        for dir in list_to_delete_segments:
-            if dir in list_saved_segments:
-                try:
-                    shutil.rmtree(save_dir + "/" + dir)
-                except OSError as e:
-                    print("Error: %s : %s" % (dir, e.strerror))
-        print("Done!")
+    print("################# Processing is Finished ;) #################")
+    print("Number of processed segments: {}".format(iteration))
