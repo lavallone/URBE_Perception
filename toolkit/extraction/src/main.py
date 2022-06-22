217e76676dc3293effc04eb726c24c5e4d4c2e32
lavallone <leo.lava99@gmail.com>
2022-06-22 11:11:27 +0200

    s

--- toolkit/extraction/src/main.py
+++ toolkit/extraction/src/main.py
@@ -1,15 +1,19 @@
 import argparse
 import waymo
 
-if __name__=="main":
+if __name__=="__main__":
     
     parser = argparse.ArgumentParser()
-    #parser.add_argument("dataset", type=str, default="waymo")
-    #args = parser.parse_args()
-
-    tfrecord_dir = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/train/prova"
-    images_dir = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/train/videos"
-    labels_json = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/labels/train/train.json"
+    parser.add_argument("dataset", type=str, default="waymo")
+    args = parser.parse_args()
+    
+    if args.dataset == "waymo":
+        tfrecord_dir = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/train/prova"
+        images_dir = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/train/videos"
+        labels_json = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/labels/train/train.json"
         
-    toolkit = waymo.WaymoToolKit(tfrecord_dir=tfrecord_dir, images_dir=images_dir, labels_json=labels_json, image_or_label="image")
-    toolkit.waymo_extraction()
+        toolkit = waymo.WaymoToolKit(tfrecord_dir=tfrecord_dir, images_dir=images_dir, labels_json=labels_json, image_or_label="image")
+        toolkit.waymo_extraction()
+    else:
+        print("porco")
+        pass
