45ae50ea4a2009ac01172375d5766826e7ab1576
lavallone <leo.lava99@gmail.com>
2022-07-19 19:03:41 +0200

    s

--- toolkit/extraction/extract.py
+++ toolkit/extraction/extract.py
@@ -42,12 +42,12 @@ class ExtractionToolkit:
         self.images_list = waymo_list + bdd100k_list + argoverse_list
         print("Now the images are: {}".format(len(self.images_list)))
         
-        for file_name in self.images_list[:10]:# mi sa non serve
-            id = self.images_lookup_table[file_name]
-            shutil.copy(file_name,"/content/drive/MyDrive/VISIOPE/Project/data/images")
-            i = [i for i,c in enumerate(file_name[::-1]) if c=="/"][0]
-            im = file_name[len(file_name)-i:]
-            os.rename("/content/drive/MyDrive/VISIOPE/Project/data/images/"+im, "/content/drive/MyDrive/VISIOPE/Project/data/images/"+id+".jpg")
+        #for file_name in self.images_list[:10]:# mi sa non serve
+        #    id = self.images_lookup_table[file_name]
+        #    shutil.copy(file_name,"/content/drive/MyDrive/VISIOPE/Project/data/images")
+        #    i = [i for i,c in enumerate(file_name[::-1]) if c=="/"][0]
+        #    im = file_name[len(file_name)-i:]
+        #    os.rename("/content/drive/MyDrive/VISIOPE/Project/data/images/"+im, "/content/drive/MyDrive/VISIOPE/Project/data/images/"+id+".jpg")
         
         
     def extract_labels(self):
