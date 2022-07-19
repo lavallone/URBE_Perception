05395adc33149ec3e82d3413d768b53c4099f69d
lavallone <leo.lava99@gmail.com>
2022-07-20 01:58:55 +0200

    s

--- toolkit/extraction/extract.py
+++ toolkit/extraction/extract.py
@@ -12,28 +12,34 @@ class ExtractionToolkit:
         self.images_list = images_list # all'inizio è 'None'
         
     def extract_images(self):
-        #print("ollllllll")
+        print("Starting extracting images...")
+        
         # first of all, we delete the previous images inside the folder
+        print("Deleting the previous images...")
         for f in glob.glob('{}/*.jpg'.format("/content/drive/MyDrive/VISIOPE/Project/data/images"), recursive=True):
             os.remove(f)
+        print("Done!")
         
         # we select images because many of them are similar (subsequent frame images)
         waymo_list = []
         bdd100k_list = []
         argoverse_list = []
         
+        print("Processing Waymo images...")
         for v in os.listdir("/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/videos"):
             video_folder = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/images/videos/"+v
             images_list = sorted(os.listdir(video_folder))
             for i in range(0,len(images_list), 6):
                 waymo_list = waymo_list + [video_folder+"/"+images_list[i], video_folder+"/"+images_list[i+1], video_folder+"/"+images_list[i+2]]
         
+        print("Processing BDD100K images...")
         for v in os.listdir("/content/drive/MyDrive/VISIOPE/Project/datasets/BDD100K/images/videos/"):
             video_folder = "/content/drive/MyDrive/VISIOPE/Project/datasets/BDD100K/images/videos/"+v+"/"
             images_list = sorted(os.listdir(video_folder))
             for i in range(0, len(images_list), 2):
                 bdd100k_list.append(video_folder+images_list[i])
                 
+        print("Processing Argoverse images...")
         for v in os.listdir("/content/drive/MyDrive/VISIOPE/Project/datasets/Argoverse/images/videos/"):
             video_folder = "/content/drive/MyDrive/VISIOPE/Project/datasets/Argoverse/images/videos/"+v+"/"
             images_list = sorted(os.listdir(video_folder))
@@ -44,27 +50,33 @@ class ExtractionToolkit:
         random.shuffle(self.images_list) # for shuffling the order of the images
         print("Now the images are: {}".format(len(self.images_list)))
         
+        print("Saving the new images to 'data/images'...")
         for file_name in self.images_list[:10]:
             id = self.images_lookup_table[file_name]
             shutil.copy(file_name,"/content/drive/MyDrive/VISIOPE/Project/data/images")
             i = [i for i,c in enumerate(file_name[::-1]) if c=="/"][0]
             im = file_name[len(file_name)-i:]
             os.rename("/content/drive/MyDrive/VISIOPE/Project/data/images/"+im, "/content/drive/MyDrive/VISIOPE/Project/data/images/"+id+".jpg")
-        
+        print("Done!")
         
     def extract_labels(self):
+        print("Starting extracting images...")
         coco_waymo = COCO("/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo/labels/COCO/annotations.json")
         coco_bdd100k = COCO("/content/drive/MyDrive/VISIOPE/Project/datasets/BDD100K/labels/COCO/annotations.json")
         coco_argoverse = COCO("/content/drive/MyDrive/VISIOPE/Project/datasets/Argoverse/labels/COCO/annotations.json")
         images = coco_waymo.dataset["images"] + coco_bdd100k.dataset["images"] + coco_argoverse.dataset["images"]
         annotations = coco_waymo.dataset["annotations"] + coco_bdd100k.dataset["annotations"] + coco_argoverse.dataset["annotations"] 
         
+        print("Retrieving the 'image_ids'...")
         image_ids_list = []
         for file_name in self.images_list:
             for im in images:
                 if im["file_name"] == file_name:
                     image_ids_list.append(im["id"])
                     break
+        print("Done!")
+        
+        print("Saving the new annotations files to 'data/labels'...")
         #d = {}
         for file_name, image_id in zip(self.images_list[:10], image_ids_list[:10]):
             value = list( map(lambda y: str(y["category_id"])+" "+str(y["bbox"][0]+" "+str(y["bbox"][1])+" "+str(y["bbox"][2]))+" "+str(y["bbox"][3]), list(filter(lambda x: x["image_id"]==image_id, annotations))) )
@@ -73,6 +85,8 @@ class ExtractionToolkit:
             f = open("/content/drive/MyDrive/VISIOPE/Project/data/labels/"+id+".txt", "w")
             f.write('\n'.join(value))
             f.close
+        print("Done!")
+
             
         #for file_name in self.images_list[:10]:
         #    id = self.images_lookup_table[file_name]
--- toolkit/extraction/main.py
+++ toolkit/extraction/main.py
@@ -1,12 +1,50 @@
-import argparse
+import os
+import json
+from pathlib import Path
 from extract import ExtractionToolkit
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("images_lookup_table", type=dict)
-    #parser.add_argument("image_or_label", type=str)
-    args = parser.parse_args()
+def uniqueid():
+    seed = 0 #random.getrandbits(20)
+    while True:
+       yield seed
+       seed += 1
+
+def images_lookup_table_create():
     
-    toolkit = ExtractionToolkit(images_lookup_table=args.images_lookup_table)
+  waymo = "/content/drive/MyDrive/VISIOPE/Project/datasets/Waymo"
+  bdd100k = "/content/drive/MyDrive/VISIOPE/Project/datasets/BDD100K"
+  argoverse = "/content/drive/MyDrive/VISIOPE/Project/datasets/Argoverse"
+
+  id_generator = uniqueid()
+  l = [waymo, bdd100k, argoverse]
+  images_lookup_table = {}
+  file_name_list = []
+  for dir in l:
+    for v in os.listdir(dir+"/images/videos"):
+      video_folder = dir+"/images/videos/"+v
+      for im in os.listdir(video_folder):
+        file_image = video_folder + "/" + im
+        id = str(next(id_generator))
+        file_name_list.append(file_image)
+        images_lookup_table[file_image] = id
+
+  print(len(images_lookup_table.keys()))
+
+  # We also write it in a json file for future uses.
+  f = open("/content/drive/MyDrive/VISIOPE/Project/data/images_lookup_table.json", "w")
+  json.dump(images_lookup_table, f)
+  f.close()
+
+  return images_lookup_table
+
+if __name__=="__main__":
+    
+    images_lookup_table = None
+    file_path = Path("/content/drive/MyDrive/VISIOPE/Project/data/images_lookup_table.json")
+    if file_path.is_file(): # if the file exists
+        images_lookup_table = json.load(open("/content/drive/MyDrive/VISIOPE/Project/datasets/Argoverse/labels/old_train.json"))
+    else:
+        images_lookup_table = images_lookup_table_create()
+    toolkit = ExtractionToolkit(images_lookup_table=images_lookup_table)
     toolkit.extract_images()
     #toolkit.extract_labels()
