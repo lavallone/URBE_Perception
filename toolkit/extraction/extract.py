9e370eab6ed878e8ba2f9780db8159075a58e1b4
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-11-05 18:29:39 +0100

    s

--- toolkit/extraction/extract.py
+++ toolkit/extraction/extract.py
@@ -166,27 +166,19 @@ class ExtractionToolkit:
         self.images_list = self.images_list[step:]
         
         print("Saving the new images to 'data/images'...")
-        image_list = []
         for file_name in tqdm(self.images_list[:5]):
             step+= 1 
             id = self.img2id[file_name]
             name = name_id(id, 6)
-            name += '.jpg'
+            name = str(step) + name + '.jpg'
             
             im = Image.open(file_name)
             resized_im = im.resize((1280, 720))
             final_im = resized_im.convert("RGB")
-            print(final_im)
-            image_list.append(final_im)
-            print(image_list[step-1])
-            #final_im.save('/content/drive/MyDrive/VISIOPE/Project/data/images/'+ name)
+            final_im.save('/content/drive/MyDrive/VISIOPE/Project/data/images/'+ name)
             self.processed_images_so_far["images_so_far"].append(step)
-            if step % 5 == 0: # we actually save the images
-              print(image_list)
-              for f_i in image_list:
-                 f_i.save('/content/drive/MyDrive/VISIOPE/Project/data/images/'+ name)
-              #image_list = []
-              f = open("/content/drive/MyDrive/VISIOPE/Project/data/processed_images_so_far.json", "w")
-              json.dump(self.processed_images_so_far, f)
-              f.close()
+            #if step % 5 == 0: # we actually save the images
+            #  f = open("/content/drive/MyDrive/VISIOPE/Project/data/processed_images_so_far.json", "w")
+            #  json.dump(self.processed_images_so_far, f)
+            #  f.close()
         print("Done!")
