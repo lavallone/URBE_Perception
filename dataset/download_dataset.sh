3b7fba1bd21d01104ed6878a4108f7deef7cbad3
lavallone <lavalle.1838492@studenti.uniroma1.it>
2023-03-07 12:09:07 +0100

    changing downloadable link

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,3 +1,3 @@
-gdown 1pSiqM_ecouNO0E_SVK_YGwlpyAwcPCU-
-unzip -q URBE_dataset_light.zip
-rm URBE_dataset_light.zip
+gdown 11-f-TWrJ2JQIDeS-QptjnzRK9hRDASi5
+unzip -q URBE_dataset_10000.zip
+rm URBE_dataset_10000.zip
